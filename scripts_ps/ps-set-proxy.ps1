66ce85146c32067a9ae30d81a642494f7c6edafb
altusea <114981887+altusea@users.noreply.github.com>
2025-10-28 20:23:49 +0800

    Update proxy environment variables in ps-set-proxy.ps1

---
+++ scripts_ps/ps-set-proxy.ps1
@@ -0,0 +1,1 @@
+$Env:http_proxy = "http://127.0.0.1:40290"; $Env:https_proxy = "http://127.0.0.1:40290"
--- scripts_ps/ps_set_proxy.ps1
+++
@@ -1,2 +0,0 @@
-$env:HTTP_PROXY="http://127.0.0.1:60290"; $env:HTTPS_PROXY="http://127.0.0.1:60290"
-
